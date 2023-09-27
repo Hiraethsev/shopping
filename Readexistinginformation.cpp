9d70b20ddd5644bb3f4733a6febda6dd457fdc65
Sefarina Lin <1353854138@qq.com>
2023-09-27 16:06:14 +0800

    first commit

---
+++ Readexistinginformation.cpp
@@ -0,0 +1,35 @@
+#include"inputinformation.cpp"
+
+using namespace std;
+
+
+void RetrieveInformation(GoodsList &L)
+{
+    GoodsList p,q;
+    int i,num;
+    FILE * myfile=fopen("D:\\华中师范大学\\编程\\c++\\goods.txt","r");
+    
+    if(myfile==NULL)
+    {
+        cerr<<"Error!"<<endl;
+        exit(1);
+    }
+
+    InitList(L);
+    q=L;
+
+    rewind(myfile);
+    num=Exportnum();
+    for(i=1;i<=num;i++)
+    {
+         p=(GoodsList)malloc(sizeof(SupermarketProduct));
+         fscanf(myfile,"%s%s",p->name,p->number);
+         fscanf(myfile,"%f,%f,%d,%d,%d,%d",&p->openprice,&p->sellprice,&p->amount,&p->date.year,&p->date.month,&p->date.day);
+         q->next=p;
+         q=q->next;
+    }
+     p->next=NULL;
+
+    fclose(myfile);
+    
+}
